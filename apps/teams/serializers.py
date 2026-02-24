9131c8185a615cdafef78bcfa0926732cd700b7a
Kanysh <kanyshomirzak@gmai.com>
2026-02-25 02:04:11 +0500

    feat: enhance Team model and add serializers for TeamStandings also make views and filters for team model

---
+++ apps/teams/serializers.py
@@ -0,0 +1,68 @@
+from rest_framework import serializers
+
+from apps.teams.models import TeamStandings, Team
+
+
+class TeamStandingsSerializer(serializers.ModelSerializer):
+    tournament_name = serializers.CharField(source="tournament.name", read_only=True)
+
+    class Meta:
+        model = TeamStandings
+        fields = [
+            "team",
+            "tournament",
+            "tournament_name",
+            "points",
+            "position",
+            "wins",
+            "podiums",
+            "def_count",
+        ]
+
+
+class TeamListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Team
+        fields = ["id", "name", "short_name", "logo", "slug", "country"]
+
+
+class TeamDetailSerializer(serializers.ModelSerializer):
+    standings = TeamStandingsSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Team
+        fields = [
+            "id",
+            "name",
+            "slug",
+            "short_name",
+            "logo",
+            "country",
+            "founded_year",
+            "description",
+            "budget",
+            "budget_currency",
+            "main_sponsor",
+            "secondary_sponsor",
+            "standings",
+            "created_at",
+            "updated_at",
+            "standings",
+        ]
+
+
+class TeamWriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Team
+        fields = [
+            "name",
+            "short_name",
+            "logo",
+            "country",
+            "founded_year",
+            "description",
+            "budget",
+            "budget_currency",
+            "main_sponsor",
+            "secondary_sponsor",
+        ]
