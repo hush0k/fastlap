7783a31e442c1c6b067b617563842916172c6c41
Scarlet Scarf <156634569+smileBlueRose@users.noreply.github.com>
2026-02-24 17:55:07 +0500

    feat: add Tag model

--- apps/news/models.py
+++ apps/news/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from apps.common.models import BaseModel
+from apps.common.mixins import NameMixin
 
-# Create your models here.
+
+class Tag(NameMixin, BaseModel):
+    class Meta:
+        verbose_name = "Tag"
+        verbose_name_plural = "Tags"
+
+    def __str__(self) -> str:
+        return self.name
