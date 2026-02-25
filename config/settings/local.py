edc8c33154d0390dba92792c9e482b1a3306a477
Scarlet Scarf <156634569+smileBlueRose@users.noreply.github.com>
2026-02-26 00:54:27 +0500

    chore: add better logging

--- config/settings/local.py
+++ config/settings/local.py
@@ -2,11 +2,11 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-INSTALLED_APPS += ['debug_toolbar']
-MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
+INSTALLED_APPS += ["debug_toolbar"]
+MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
 
-INTERNAL_IPS = ['127.0.0.1']
+INTERNAL_IPS = ["127.0.0.1"]
