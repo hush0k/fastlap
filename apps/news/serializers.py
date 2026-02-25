2541a16b676e2b944487f7d8ea694696d4987259
Scarlet Scarf <156634569+smileBlueRose@users.noreply.github.com>
2026-02-25 18:07:06 +0500

    feat: update ArticleListSerializer

--- apps/news/serializers.py
+++ apps/news/serializers.py
@@ -1,9 +1,25 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
-from .models import Article
+from .models import Article, Tag
+
+
+class TagSerializer(ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ["id", "name"]
 
 
 class ArticleListSerializer(ModelSerializer):
+    author = SerializerMethodField()
+    tags = TagSerializer(many=True)
+    series = SerializerMethodField()
+
+    def get_series(self, obj: Article) -> list[dict[str, int | str]]:
+        return [dict(id=i.id, name=i.name) for i in obj.series.all()]
+
+    def get_author(self, obj: Article) -> dict[str, int | str]:
+        return dict(id=obj.author.id, username=obj.author.username)
+
     class Meta:
         model = Article
         fields = "__all__"
