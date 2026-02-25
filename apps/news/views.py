d9fa773ecf6bd592dce776223d4386e1726361ba
Scarlet Scarf <156634569+smileBlueRose@users.noreply.github.com>
2026-02-25 18:08:29 +0500

    feat: add more filter and refactor filter with DjangoFilter

--- apps/news/views.py
+++ apps/news/views.py
@@ -1,11 +1,13 @@
 from logging import getLogger
 
-from django.db.models import QuerySet
+from django_filters.rest_framework.backends import DjangoFilterBackend
+
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from apps.common.pagination import CustomPagination
 
+from .filters import ArticleFilter
 from .models import Article
 from .serializers import ArticleListSerializer
 
@@ -17,18 +19,5 @@ class ArticleListView(generics.ListAPIView):
     serializer_class = ArticleListSerializer
     queryset = Article.objects.all()
     pagination_class = CustomPagination
-
-    def get_queryset(self) -> QuerySet[Article]:
-        q = Article.objects.all()
-
-        author_id: str | None = self.request.query_params.get("author_id")
-        logger.debug("author_id: %r", author_id)
-        if author_id is not None and author_id.isdigit():
-            q = q.filter(author_id=int(author_id))
-
-        series: list[str] = self.request.query_params.getlist("series")
-        logger.debug("series: %r", series)
-        if series:
-            q = q.filter(series__name__in=series)
-
-        return q
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ArticleFilter
