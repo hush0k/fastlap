62381a335dc38eca44c780e8d310e6ab7a9a1614
Alrami <a.synthux@gmail.com>
2026-02-25 23:06:02 +0500

    feat: add DriverFilter and DriverResultFilter

---
+++ apps/drivers/filters.py
@@ -0,0 +1,31 @@
+from django.db.models import Q, QuerySet
+from django_filters import BaseInFilter, BooleanFilter, CharFilter, FilterSet, NumberFilter
+
+from .models import Driver, DriverResult
+
+
+class DriverFilter(FilterSet):
+    nationality = BaseInFilter(field_name="nationality")
+    search = CharFilter(method="filter_search")
+    is_active = BooleanFilter(field_name="is_active")
+    number = NumberFilter(field_name="number")
+
+    def filter_search(self, queryset: QuerySet[Driver], name: str, value: str) -> QuerySet[Driver]:
+        return queryset.filter(Q(first_name__icontains=value) | Q(last_name__icontains=value))
+    
+    class Meta:
+        model = Driver
+        fields = ["slug", "nationality", "is_active", "number"]
+
+
+class DriverResultFilter(FilterSet):
+    status = BaseInFilter(field_name="status")
+    fastest_lap = BooleanFilter(field_name="fastest_lap")
+    driver = NumberFilter(field_name="driver__id")
+    race = NumberFilter(field_name="race__id")
+    season = NumberFilter(field_name="race__season__id")
+    series = NumberFilter(field_name="race__series__id")
+
+    class Meta:
+        model = DriverResult
+        fields = ["driver", "race", "status", "fastest_lap", "season", "series"]
