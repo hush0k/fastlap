f9aea830d5f2b49fb8ade18edaf3066febbffd36
Scarlet Scarf <156634569+smileBlueRose@users.noreply.github.com>
2026-02-26 00:54:27 +0500

    chore: add better logging

--- middleware/requests.py
+++ middleware/requests.py
@@ -1,8 +1,11 @@
 import uuid
 from contextvars import ContextVar
+from logging import getLogger
 from typing import Callable
 
 from django.http import HttpRequest, HttpResponse
+
+logger = getLogger(__name__)
 
 request_id_var: ContextVar[str] = ContextVar("request_id", default="")
 
@@ -19,3 +22,18 @@ class RequestIDMiddleware:
         response["X-Request-ID"] = request_id
 
         return response
+
+
+class RequestLoggingMiddleware:
+    def __init__(self, get_response: Callable[[HttpRequest], HttpResponse]) -> None:
+        self.get_response = get_response
+
+    def __call__(self, request: HttpRequest) -> HttpResponse:
+        logger.debug(
+            '"%s %s" | %s | %s |',
+            request.method,
+            request.path,
+            request.META.get("REMOTE_ADDR"),
+            request.META.get("HTTP_USER_AGENT"),
+        )
+        return self.get_response(request)
