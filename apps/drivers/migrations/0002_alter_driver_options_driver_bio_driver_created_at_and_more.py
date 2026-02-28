a25dcafa8c73d23a2a5aa245ccf4817a36e6f3db
Alrami <a.synthux@gmail.com>
2026-03-01 00:35:39 +0500

    Revert "Reapply "Reapply "Reapply "Fix the migrations""""
    This reverts commit eded0305dc5f6a061114c93ea1edcd5074ca8ac5.

--- apps/drivers/migrations/0002_alter_driver_options_driver_bio_driver_created_at_and_more.py
+++ apps/drivers/migrations/0002_alter_driver_options_driver_bio_driver_created_at_and_more.py
@@ -55,7 +55,7 @@ class Migration(migrations.Migration):
             model_name="driver",
             name="nationality",
             field=django_countries.fields.CountryField(
-                default="en", max_length=2, verbose_name="Nationality"
+                default="default-nationality", max_length=2, verbose_name="Nationality"
             ),
             preserve_default=False,
         ),
