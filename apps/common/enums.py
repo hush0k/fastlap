ebb6e73117aefe0bd183a1af34e3c29d3f4595b1
Scarlet Scarf <156634569+smileBlueRose@users.noreply.github.com>
2026-03-01 10:23:47 +0500

    feat: add Currency enum

--- apps/common/enums.py
+++ apps/common/enums.py
@@ -1,4 +1,4 @@
-from enum import StrEnum
+from enum import StrEnum, Enum
 
 
 class SeriesCategoryEnum(StrEnum):
@@ -21,3 +21,36 @@ class WatchPlatformEnum(StrEnum):
     DAZN = "dazn"
     ESPN = "espn"
     SKY_SPORTS = "sky_sports"
+
+
+class Currency(Enum):
+    """
+    Enum of popular currencies.
+
+    Each member is a tuple of (code, verbose_name).
+    Use `.code` to get the ISO 4217 currency code and `.verbose` for the human-readable name.
+    """
+
+    USD = ("USD", "US Dollar")
+    EUR = ("EUR", "Euro")
+    GBP = ("GBP", "British Pound")
+    JPY = ("JPY", "Japanese Yen")
+    CNY = ("CNY", "Chinese Yuan")
+    KRW = ("KRW", "South Korean Won")
+    RUB = ("RUB", "Russian Ruble")
+    KZT = ("KZT", "Kazakhstani Tenge")
+    CAD = ("CAD", "Canadian Dollar")
+    AUD = ("AUD", "Australian Dollar")
+    CHF = ("CHF", "Swiss Franc")
+    SEK = ("SEK", "Swedish Krona")
+    BRL = ("BRL", "Brazilian Real")
+    INR = ("INR", "Indian Rupee")
+    SGD = ("SGD", "Singapore Dollar")
+
+    @property
+    def code(self) -> str:
+        return self.value[0]
+
+    @property
+    def verbose(self) -> str:
+        return self.value[1]
