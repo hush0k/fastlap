e8641401acfc280878d1441ef49deca2a4d8646d
Scarlet Scarf <156634569+smileBlueRose@users.noreply.github.com>
2026-03-03 17:56:06 +0500

    feat: add GET /tournaments endpoint. - add tournaments filterset - add Series serializer - add tournament serializer

--- apps/tournaments/views.py
+++ apps/tournaments/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from typing import Any
 
-# Create your views here.
+from django_filters.rest_framework.backends import DjangoFilterBackend
+
+from django.http import HttpRequest, HttpResponse
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+
+from apps.common.pagination import CustomPagination
+
+from .filters import TournamentFilter
+from .models import Tournament
+from .serializers import TournamentListSerializer
+
+
+class TournamentListView(generics.ListAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = TournamentListSerializer
+    queryset = Tournament.objects.filter(is_active=True)
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = TournamentFilter
+    pagination_class = CustomPagination
+
+
+class TournamentView(APIView):
+    def dispatch(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        if request.method == "GET":
+            handler = TournamentListView.as_view()
+        else:
+            return self.http_method_not_allowed(request, *args, **kwargs)
+
+        return handler(request, *args, **kwargs)
