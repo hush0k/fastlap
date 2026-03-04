7f276c68e37bb73ddb578b4e6123a188bb2f2a3a
Scarlet Scarf <156634569+smileBlueRose@users.noreply.github.com>
2026-03-04 17:04:57 +0500

    fix: create permissions before set it to the group

--- apps/news/migrations/0002_create_author_group.py
+++ apps/news/migrations/0002_create_author_group.py
@@ -1,7 +1,15 @@
+from django.apps import apps as global_apps
+from django.contrib.auth.management import create_permissions
+from django.contrib.contenttypes.management import create_contenttypes
 from django.db import migrations
 
 
 def create_author_group(apps, schema_editor):
+    app_config = global_apps.get_app_config("tournaments")
+
+    create_contenttypes(app_config, verbosity=0)
+    create_permissions(app_config, verbosity=0)
+
     Group = apps.get_model("auth", "Group")
     Permission = apps.get_model("auth", "Permission")
 
