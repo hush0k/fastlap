5ca9989c53788521bc2d1d59149cf91e0b4dcff0
Kanysh <kanyshomirzak@gmai.com>
2026-05-11 17:38:41 +0500

    feat: update setup script for FastLap project with improved commands and messaging

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-echo "Setting up the development environment..."
+echo "Setting up FastLap project..."
 
 cp .env.example .env
 
-docker-compose up --build -d
+docker compose down -v
 
+docker compose up --build -d
+
+echo "Waiting for database..."
 sleep 5
 
-docker-compose exec web python manage.py migrate
+docker compose exec web python manage.py migrate
 
+docker compose exec web python manage.py seed_db
 
+docker compose exec web python manage.py createsuperuser
+
+echo "Done! Project is running at http://localhost:8000"
+echo "API docs: http://localhost:8000/api"
