59b4aa8e539e5f93e5e7defc2bbf2702d5d3b515
K1yotakaaa <jeskozee@gmail.com>
2026-05-11 18:00:38 +0500

    feat(race_tracks): enhance admin interface

--- apps/race_tracks/admin.py
+++ apps/race_tracks/admin.py
@@ -1,2 +1,48 @@
+"""
+Admin configuration for the race_tracks app.
+"""
 
-# Register your models here.
+# Django modules
+from django.contrib import admin
+
+# Project modules
+from apps.race_tracks.models import Track
+
+
+@admin.register(Track)
+class TrackAdmin(admin.ModelAdmin):
+  """
+  Admin interface for Track model.
+  """
+
+  list_display = (
+    "id",
+    "name",
+    "slug",
+    "country",
+    "city",
+    "timezone",
+    "length_km",
+    "lap_record",
+    "lap_record_holder",
+    "number_of_turns",
+  )
+  list_filter = ("country", "timezone")
+  search_fields = ("name", "city", "country")
+  readonly_fields = ("created_at", "updated_at", "slug")
+  ordering = ("name", "country")
+  fieldsets = (
+    ("Basic Information", {
+      "fields": ("name", "slug", "country", "city", "timezone")
+    }),
+    ("Track Specifications", {
+      "fields": ("length_km", "number_of_turns", "map_image")
+    }),
+    ("Records", {
+      "fields": ("lap_record", "lap_record_holder")
+    }),
+    ("Timestamps", {
+      "fields": ("created_at", "updated_at"),
+      "classes": ("collapse",)
+    }),
+  )
