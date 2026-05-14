f2e389c6867e5de92fee2831627efedca1cab704
Alrami <a.synthux@gmail.com>
2026-05-14 21:53:40 +0500

    feat: Added config/routers.py to manage WebSocket path routes.

---
+++ config/routing.py
@@ -0,0 +1,10 @@
+# from django.urls import path
+
+# from apps.chat.consumers import ChatConsumer
+# from apps.tournaments.consumers import LiveTelemetryConsumer
+
+websocket_urlpatterns = [
+    # path("path", Consumer.as_asgi()),
+    # path("ws/chat/", ChatConsumer.as_asgi()),
+    # path("ws/tournaments/live-telemetry/", LiveTelemetryConsumer.as_asgi()),
+]
