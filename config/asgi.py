f64e03a19e96d564118c97c289ef43d4209f00d4
Peli <a.synthux@gmail.com>
2026-05-14 22:14:04 +0500

    Websocket (#34)
    * feat: Added channels, daphne, and channels-redis
    
    * feat: Added daphne to the top of INSTALLED_APPS; Defined the ASGI_APPLICATION setting
    
    * feat: Refactored to use ProtocolTypeRouter.
    
    * feat: Added config/routers.py to manage WebSocket path routes.

--- config/asgi.py
+++ config/asgi.py
@@ -9,8 +9,22 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+import config.routing
 
-application = get_asgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+django_asgi_app = get_asgi_application()
+
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AuthMiddlewareStack(
+            URLRouter(config.routing.websocket_urlpatterns)
+        ),
+    }
+)
