a3c0c5a0d3c773b698de79d34f94a3ef2200ba17
Smile <156634569+smileBlueRose@users.noreply.github.com>
2026-05-16 04:55:28 +0500

    refactor: use black and ruff

--- apps/teams/models.py
+++ apps/teams/models.py
@@ -1,5 +1,6 @@
-from django.db import models
 from django_countries.fields import CountryField
+
+from django.db import models
 
 from apps.common.mixins import CreatedAtMixin, NameMixin, UpdatedAtMixin
 
