899c6efe53a9a9a998e92b6a74e14b7da8c60a1d
Smile <156634569+smileBlueRose@users.noreply.github.com>
2026-05-16 11:29:35 +0500

    feat: add celery task

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,8 @@
 
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+celery -A config worker -l INFO &
+celery -A config beat -l INFO &
+
 gunicorn --bind 0.0.0.0:8000 --workers 3 config.wsgi:application
